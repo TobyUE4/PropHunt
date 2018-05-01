5eb87e933ee678c176e1237951f24c11243f5910
TobyUE4 <36339200+TobyUE4@users.noreply.github.com>
2018-05-01 19:42:23 -0400

    Get Debug Lines Working
    The framework for ray casting has now been layed.

--- Source/PropHunt/Private/PropPC.cpp
+++ Source/PropHunt/Private/PropPC.cpp
@@ -5,5 +5,35 @@
 #include "PropPC.h"
 
 
-void APropPC::BecomeProp() {}
+void APropPC::BecomeProp() {
+
+	UE_LOG(LogTemp, Warning, TEXT("You want to become a prop"));
+
+	int32 ViewportX, ViewPortY;
+	GetViewportSize(ViewportX, ViewPortY);
+
+	FVector2D ScreenLocation = FVector2D((ViewportX * .5), ((ViewPortY * .58) - 20));
+
+	FVector WorldDirection;
+	FVector WorldLocation;
+
+	DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, WorldLocation, WorldDirection);
+
+	FVector StartLocation = PlayerCameraManager->GetCameraLocation();
+	FVector EndLocation = StartLocation + (WorldDirection * 100000);
+
+	DrawDebugLine(
+
+		GetWorld(),
+		StartLocation,
+		EndLocation,
+		FColor(255, 0, 0),
+		true,
+		60.f,
+		0,
+		1.f
+
+	);
+
+}
 
