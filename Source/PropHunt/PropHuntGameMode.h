1f2373538ee845e4aa0660af587d2f15bdd10191
TobyUE4 <36339200+TobyUE4@users.noreply.github.com>
2018-05-01 16:42:44 -0400

    Initial commit

---
+++ Source/PropHunt/PropHuntGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "PropHuntGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROPHUNT_API APropHuntGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
