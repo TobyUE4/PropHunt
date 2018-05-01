cc62e69bcf7c87580355b8e4289b919001e6172b
TobyUE4 <36339200+TobyUE4@users.noreply.github.com>
2018-05-01 18:20:22 -0400

    Get pointer logging working
    You can set it. It knows when nothing is there, but it seems it isn't working if you do set a pointer.

--- Source/PropHunt/Private/Prop.cpp
+++ Source/PropHunt/Private/Prop.cpp
@@ -10,6 +10,10 @@ AProp::AProp()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	if (ModelToSet) { UE_LOG(LogTemp, Warning, TEXT("Prop model set!")); }
+
+	else { UE_LOG(LogTemp, Warning, TEXT("Error- a prop is missing a model")); }
+
 }
 
 // Called when the game starts or when spawned
